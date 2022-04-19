5554c8c8006d9fa32fd920fbbfc6fc0e5dd58602
tyler-morales <tyler.morales.dev@gmail.com>
2022-04-19 18:48:59 -0500

    💄  Add font family Mukta

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div></div>
+  return (
+    <div>
+      <h1 className="text-5xl font-[700]">
+        Landscape maintenance & design/ builds
+      </h1>
+    </div>
+  )
 }
