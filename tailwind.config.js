895350030a5760de87399f085ac679eda6688daa
tyler-morales <tyler.morales.dev@gmail.com>
2022-04-19 18:41:12 -0500

    💄  Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#08293A',
+        secondary: '#FFC82C',
+      },
+    },
   },
   plugins: [],
 }
