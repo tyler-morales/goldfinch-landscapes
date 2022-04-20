6b0c97cb5af8f0de288e39e5e1f8166f326dee1e
tyler-morales <tyler.morales.dev@gmail.com>
2022-04-20 13:08:33 -0500

    ✨  Add paragraph Who we are statement

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,15 @@ export default function Home() {
   return (
     <>
       <Nav />
-      <Hero />
+      <main>
+        <Hero />
+        <section className="bg-gray-200">
+          <p className="max-w-xl p-6 py-10 m-auto text-2xl font-bold leading-10 text-center">
+            We’re the name of the game when it comes to high quality and
+            friendly landscape services
+          </p>
+        </section>
+      </main>
     </>
   )
 }
