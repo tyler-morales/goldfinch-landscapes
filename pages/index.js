cdcc294b09c11cb1333a96156aa6930cea1b297d
tyler-morales <tyler.morales.dev@gmail.com>
2022-04-20 11:38:17 -0500

    🏗️  Set html to default <div>

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-5xl font-[700]">
-        Landscape maintenance & design/ builds
-      </h1>
-    </div>
-  )
+  return <div></div>
 }
