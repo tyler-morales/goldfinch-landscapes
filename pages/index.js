bc3a0f9a33d3529f8e78fedcb26ae9e5629f1ce8
tyler-morales <tyler.morales.dev@gmail.com>
2022-04-27 15:36:36 -0500

    🗃️  Add meta data

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,42 @@ import Nav from '../components/Nav'
 import Portfolio from '../components/Portfolio'
 import Services from '../components/Services'
 
+import Head from 'next/head'
+
 export default function Home() {
   return (
     <>
+      <Head>
+        {/* Title */}
+        <title>
+          Goldfinch Landscapes | Maintance and Design/ Build Landscapes in
+          Southwest Michigan
+        </title>
+        <meta
+          property="og:title"
+          content="Goldfinch Landscapes | Maintance and Design/ Build Landscapes in Southwest Michigan"
+        />
+
+        {/* Description */}
+        <meta
+          name="description"
+          content="Goldfinch Landscapes is a landscape maintance and design/ build company in Southwest Michigan. We offer a wide variety of landscape designs in Southwest Michigan. Located in the beautiful city of Union Pier, Michigan, we are a full service landscape company that specializes in providing you with the landscaping experience."
+        />
+
+        <meta
+          property="og:description"
+          content="Goldfinch Landscapes is a landscape maintance and design/ build company in Southwest Michigan. We offer a wide variety of landscape designs in Southwest Michigan. Located in the beautiful city of Union Pier, Michigan, we are a full service landscape company that specializes in providing you with the landscaping experience."
+        />
+
+        {/* Misc */}
+        <meta name="twitter:card" content="summary" />
+        <meta property="og:url" content="https://goldfinchlandscapes.com /" />
+        <meta
+          property="og:image"
+          content="https://raw.githubusercontent.com/tyler-morales/goldfinch-landscapes/main/public/images/image-card.png"
+        />
+        <meta property="og:type" content="website" />
+      </Head>
       <Nav />
       <main>
         <Hero />
